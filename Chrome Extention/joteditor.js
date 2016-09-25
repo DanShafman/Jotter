34764219a77f5547be07973bacfa07353dca04de
JosephBabbitt <jrebbabbitt@gmail.com>
2016-09-25 12:34:13 -0400

    Added editor option to symbols

--- Chrome Extention/joteditor.js
+++ Chrome Extention/joteditor.js
@@ -16,43 +16,26 @@ function initApp() {
   currentsnap = snapshot.val();
   console.log(currentsnap);
   $('.jotid').text('"'+id+'"');
+
+  //load stuff
+var storage = localStorage.getItem(uid+id+'notes');
+console.log(storage);
+console.log(uid+id+'notes');
+$('#textarea').val(storage);
+Materialize.toast('Alright, you\'re set. Enjoy your jot.', 4000);
+
   });
-}
 $(document).ready(function(){
-	$('.input-field').hide();
-	$('.input-field-n').hide();
-	$('#setupbtn').hide();
-	$('#setupnote').click(function(){
-		type = 1;
-		$('#setupnote').hide();
-		$('#setuplink').hide();
-		$('#setupbtn').show();
-		$('.input-field-n').show();
-	});
-	$('#setuplink').click(function(){
-		type = 2;
-		$('#setuplink').hide();
-		$('#setupnote').hide();
-		$('#setupbtn').show();
-		$('.input-field').show();
+	$('#save').click(function(){
+		var url = $('#url').val();
+		dbref.ref('/' +uid+'/Jots/'+id).update({
+			url: 'joteditor.html?id='+id
+		});
+		localStorage.setItem(uid+id+"notes", $('#textarea').val());
+		Materialize.toast('Alright, you\'re set. It has saved', 4000);
 	})
-	$('#setup').click(function(){
-		if(type==2){
-			var url = $('#url').val();
-			dbref.ref('/' +uid+'/Jots/'+id).update({
-				url: url
-			});
-		}
-		if(type==1){
-			var url = $('#url').val();
-			dbref.ref('/' +uid+'/Jots/'+id).update({
-				url: 'joteditor.html?id='+id
-			});
-			localStorage.setItem(uid+id+"notes", $('#notes').val());
-		}
-	Materialize.toast('Alright, you\'re set. Enjoy your jot.', 4000)
-	});
 });
+}
 window.onload = function() {
   initApp();
 };
